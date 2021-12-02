ebc18a1b11cc5a26eb790a47cc209555a43f01c7
Adithyanaikwade <93151163+Adithyanaikwade@users.noreply.github.com>
2021-12-02 16:13:00 +0530

    a
    a

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 for (var i = 0; i<=6;i++){
+
 document.querySelectorAll("button")[i].addEventListener("click", function(){
     var sounds = this.innerHTML;
-    console.log(sounds);
     handleClick(sounds);
 } );
 }
@@ -9,7 +9,7 @@ var obj = {
     "B": "sounds/tom-1.mp3",
     "H": "sounds/tom-2.mp3",
     "A": "sounds/tom-3.mp3",
-    "G": "sounds/tom-4.mp3",
+    "J": "sounds/tom-4.mp3",
     "A": "sounds/snare.mp3",
     "'": "sounds/crash.mp3",
     "S": "sounds/kick-bass.mp3"
@@ -20,3 +20,8 @@ function handleClick(sounds){
     let audio = new Audio(obj[sounds]);
     audio.play();
 }
+document.addEventListener("keypress", function(event){
+    let keys = event.key;
+    console.log(keys);
+    handleClick(keys);
+})
